d244483b3662921dd67fb86b49922645fb0a60f7
Stiku2137 <mciesla97@gmail.com>
2023-02-05 18:37:23 +0100

    secondtest

--- src/test/java/test/tests/firstTest.java
+++ src/test/java/test/tests/firstTest.java
@@ -1,7 +1,12 @@
 package test.tests;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class firstTest {
 
@@ -12,8 +17,17 @@ public class firstTest {
                         new BrowserType.LaunchOptions().setHeadless(false)
                         );
                 Page page = browser.newPage();
-                page.navigate("https://letcode.in/");
+                page.navigate("http://playwright.dev");
                 System.out.println(page.title());
+                System.out.println(page.url());
+
+
+                assertThat(page).hasTitle(Pattern.compile("Playwright"));
+
+                Locator getStarted = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Get Started"));
+                assertThat(getStarted).hasAttribute("href", "/docs/intro");
+                getStarted.click();
+                assertThat(page).hasURL(Pattern.compile(".*intro"));
                 page.close();
                 browser.close();
                 playwright.close();
